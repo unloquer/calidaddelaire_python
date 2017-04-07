d16edf0d9de1dd8c6d8f791e2c641857c7f6c6d4
Daniel <danielgomezmarin@gmail.com>
2017-04-07 16:04:19 +0200

    Nuevo repo con el código en python
    van tres datasets pequeños y un .py

---
+++ datos_aire.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+
+def loadJson(file):
+#cargar archivo de json con datos
+#crear un array con coordenadas y pm25 llamado dataset
+    with open(file) as json_data:
+        j = json.load(json_data)
+        #print type(j['features'])
+        #print len(j['features'])
+        dataset=[]
+        for medicion in j['features']:
+        	#print medicion
+        	coord=[]
+        	coord.append([medicion['geometry']['coordinates'][0],medicion['geometry']['coordinates'][1], medicion['properties']['pm25']])
+        	dataset.append(coord[0])
+        dataset=np.array(dataset)
+    return dataset
+
+def loadCsv(file):
+#cargar un csv con todos los datos
+#retorna un dataset con coordenadas y pm25
+    dataset=[]
+    with open(file) as rawdata:
+        for rawline in rawdata:
+            coord=[]
+            linelist=[x for x in rawline.split(',')]
+            #print linelist
+            if "INVALID" not in linelist and 'lat' not in linelist:
+                coord.append([float(linelist[0]),float(linelist[1]),float(linelist[9][:-1])])
+                #print coord[0]
+                dataset.append(coord[0])
+    dataset=np.array(dataset)
+    #print dataset
+    return dataset
+
+dataset=loadCsv('csv_brolin.txt') #usar esta linea para cargar todos los csv
+#dataset=loadJson('aire_brol2.json')#usar esta linea para cargar el jsopn
+
+#redondear las coordenadas de dataset 
+#ajustar los decimales en "dec"
+dec=3 #numero de decimales de precision de las coordenadas
+dataset[:,0]=np.around(dataset[:,0],dec) #redondear los datos x
+dataset[:,1]=np.around(dataset[:,1],dec) #redondear los datos y
+
+#buscar posiciones repetidas y guardarlas en "zet"
+pos=[(x[0],x[1]) for x in dataset]
+zet=set(tuple(i) for i in pos)
+
+#sacar la media de las coordenadas repetidas
+#guardar x,y,pm25 en diferentes listas
+
+x_map=[]
+y_map=[]
+p_map=[]
+
+for position in zet:
+	heatid=[]
+	for x in dataset:
+		if x[:2][0]==position[0]:
+			heatid.append(x[2])
+	
+	x_map.append(position[0])
+	y_map.append(position[1])
+	p_map.append(np.mean(heatid))
+
+#hacer un plot
+plt.scatter(x_map, y_map, marker="s", edgecolors="face", s=50, c=p_map, alpha=0.5)
+# plt.xlim(min(x_map),max(x_map))
+# plt.ylim(min(y_map),max(y_map))
+plt.axis('scaled')
+
+plt.show()
+
+    	
